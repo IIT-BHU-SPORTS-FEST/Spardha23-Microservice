25fa7a8d2a4f81ccb9b9bffd3bbd99e7f6056005
Varun-Kolanu <kolanuvarun739@gmail.com>
2023-10-05 12:15:24 +0530

    [ADDED] Checking of user is staff or admin in adding, editing, deleting games

--- app.js
+++ app.js
@@ -7,6 +7,14 @@ import YAML from 'yamljs';
 
 const swaggerDocument = YAML.load('./config/gameRoutes.yaml');
 export const app = express();
+app.use((req, res, next) => {
+    const authHeader = req.headers.authorization;
+    if (authHeader && authHeader.startsWith('Token ')) {
+        const token = authHeader.split(' ')[1];
+        req.token = token;
+    }
+    next();
+});
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use(express.json());
 
