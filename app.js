e4212c5930ab8eac16f234a8e920f5998f024ee4
Mradul Singh <mradul.singh.min21@itbhu.ac.in>
2023-10-10 22:08:18 +0530

    Added Cors

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ import express from "express";
 import gameRouter from "./routes/gameRouter.js";
 import { errorMiddleware } from "./middlewares/error.js";
 import swaggerUi from "swagger-ui-express";
+import cors from 'cors';
 import YAML from 'yamljs';
 
 const swaggerDocument = YAML.load('./config/gameRoutes.yaml');
 export const app = express();
+app.use(cors());
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use(express.json());
 
